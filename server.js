69ba0b75621ccf5d148655c58bd25a24167c9355
ashishrawat0 <rawatchd@hotmail.com>
2020-08-17 22:49:26 +0530

    added /api in the apis

--- server.js
+++ server.js
@@ -6,20 +6,20 @@ var controller = require('./controller/controller')
 app.use(cors())
 var router = express.Router()
 require('dotenv').config()
-app.get('/top-stories', async function (req, res) {
+app.get('/api/top-stories', async function (req, res) {
     const data = await controller.getStory()
     res.send(data)
 
 })
 
-app.get('/comments', async function (req, res) {
-    let storyId= req.query.id
-    let data =await controller.getComment(storyId)
+app.get('/api/comments', async function (req, res) {
+    let storyId = req.query.id
+    let data = await controller.getComment(storyId)
     res.status(200).send(data)
 })
 
-app.get('/past-stories',async function(req,res){
-    let data =await controller.pastStories(storyId)
+app.get('/api/past-stories', async function (req, res) {
+    let data = await controller.pastStories(storyId)
     res.status(200).send(data)
 })
 
